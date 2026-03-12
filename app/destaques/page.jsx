6839c1b6e69f50d92c7a77f50c9343ba7688bed4
Igor da Silva Dias <rogigor1712@gmail.com>
2026-03-12 18:28:45 -0300

    versão 6 (acho)
    adicionei 'destaques' melhorei responsividade, e acho que é isso

--- app/destaques/page.jsx
+++ app/destaques/page.jsx
@@ -4,49 +4,80 @@ export default function Destaques() {
     return (
         <div>
             <h1>Destaques</h1>
-            <section className="main-fotos">
-                <figure>
-                    <Image className="img" src={"/img-fosseis-certas/fossil-elasmotherium.jpeg"} alt="Fóssil de Elasmotherium" width={200} height={200} />
-                    <figcaption>Fóssil de Elasmotherium</figcaption>
+            <section className="main-fotos-real">
+                <figure className="img-animal-real">
+                    <Image className="img" src={"/animais-reais/mamute-lanoso2.webp.jpeg"} alt="Mamute-Lanoso" width={200} height={200} />
+                    <figcaption>Mamute-Lanoso</figcaption>
+                    <figcaption className="info"><strong>Nome científico:</strong> Mammuthus primigenius</figcaption>
+                    <figcaption className="info"><strong>O que era:</strong> Um tipo de elefante gigante coberto de pelos que vivia em regiões muito frias.</figcaption>
+                    <figcaption className="info">
+                        <strong>Tamanho:</strong>
+                        <ul>
+                            <li>até 4 metros de altura</li>
+                            <li>podia pesar de 6 a 8 toneladas</li>
+                        </ul>
+                    </figcaption>
+                    <figcaption className="info">
+                        <strong>Características:</strong>
+                        <ul>
+                            <li>pelagem grossa para suportar o frio</li>
+                            <li>presas curvas gigantes</li>
+                            <li>camada de gordura para proteger do gelo</li>
+                        </ul>
+                    </figcaption>
+                    <figcaption className="info"><strong>Onde vivia:</strong> Europa, Ásia e América do Norte</figcaption>
+                    <figcaption className="info"><strong>Alimentação:</strong> Era herbívoro. Comia principalmente grama, folhas, galhos e plantas da tundra.</figcaption>
+                    <figcaption className="info"><strong>Curiosidade:</strong> Alguns mamutes foram encontrados congelados no gelo da Sibéria, com pele, pelos e até restos de comida no estômago preservados.</figcaption>
                 </figure>
-                <figure>
-                    <Image className="img" src={"/img-fosseis-certas/fossil-glyptodon.jpeg"} alt="Fóssil de Glyptodon" width={200} height={200} />
-                    <figcaption>Fóssil de Glyptodon</figcaption>
+                <figure className="img-animal-real">
+                    <Image className="img" src={"/animais-reais/megaterio2.jpeg"} alt="Megatéiro" width={200} height={200} />
+                    <figcaption>Megatério</figcaption>
+                    <figcaption className="info"><strong>Nome científico:</strong> Megatherium americanum</figcaption>
+                    <figcaption className="info"><strong>O que era:</strong> Uma preguiça gigante terrestre, diferente das preguiças atuais que vivem nas árvores.</figcaption>
+                    <figcaption className="info">
+                        <strong>Tamanho:</strong>
+                        <ul>
+                            <li>até 6 metros de comprimento</li>
+                            <li>podia pesar cerca de 4 toneladas</li>
+                        </ul>
+                    </figcaption>
+                    <figcaption className="info">
+                        <strong>Características:</strong>
+                        <ul>
+                            <li>garras enormes</li>
+                            <li>corpo muito forte</li>
+                            <li>conseguia ficar em pé nas patas traseiras para alcançar folhas</li>
+                        </ul>
+                    </figcaption>
+                    <figcaption className="info"><strong>Onde vivia:</strong> Principalmente na América do Sul, inclusive no território do Brasil.</figcaption>
+                    <figcaption className="info"><strong>Alimentação:</strong> Era herbívoro. Comia folhas, frutos, raízes e galhos de árvores.</figcaption>
+                    <figcaption className="info"><strong>Curiosidade:</strong> Mesmo sendo herbívoro, suas garras enormes provavelmente serviam para defesa contra predadores.</figcaption>
                 </figure>
-                <figure>
-                    <Image className="img" id="precisa-rotacionar" src={"/img-fosseis-certas/fossil-leao-das-cavernas.jpeg"} alt="Fóssil de Leão-das-Cavernas" width={200} height={200} />
-                    <figcaption>Fóssil de filhote de Leão-das-Cavernas</figcaption>
-                </figure>
-                <figure>
-                    <Image className="img" src={"/img-fosseis-certas/fossil-lobo-terrivel.jpeg"} alt="Fóssil de Lobo-Terrível" width={200} height={200} />
-                    <figcaption>Fóssil de Lobo-Terrível</figcaption>
-                </figure>
-                <figure>
-                    <Image className="img" src={"/img-fosseis-certas/fossil-mamute-lanoso.jpeg"} alt="Fóssil de Mamute-Lanoso" width={200} height={200} />
-                    <figcaption>Fóssil de bebê de Mamute-Lanoso</figcaption>
-                </figure>
-                <figure>
-                    <Image className="img" src={"/img-fosseis-certas/fossil-mandibula-mastodonte.jpeg"} alt="Fóssil de mandíbula de um Mastodonte" width={200} height={200} />
-                    <figcaption>Fóssil de mandíbula de Mastodonte</figcaption>
-                </figure>
-                <figure>
-                    <Image className="img" src={"/img-fosseis-certas/fossil-megaterio.jpeg"} alt="Fóssil de um Megatéiro" width={200} height={200} />
-                    <figcaption>Fóssil de Megatério</figcaption>
-                </figure>
-                <figure>
-                    <Image className="img" src={"/img-fosseis-certas/fossil-rinoceronte-lanoso.jpeg"} alt="Fóssil de Rinoceronte-Lanoso" width={200} height={200} />
-                    <figcaption>Fóssil de Rinoceronte-Lanoso</figcaption>
-                </figure>
-                <figure>
-                    <Image className="img" src={"/img-fosseis-certas/fossil-tigre-dentes-de-sabre.jpeg"} alt="Fóssil de Tigre-Dentes-de-Sabre" width={200} height={200} />
-                    <figcaption>Fóssil de Tigre-Dentes-de-Sabre</figcaption>
-                </figure>
-                <figure>
-                    <Image className="img" src={"/img-fosseis-certas/fossil-urso-das-cavernas.jpeg"} alt="Fóssil de Urso-das-Cavernas" width={200} height={200} />
-                    <figcaption>Fóssil de Urso-das-Cavernas</figcaption>
+                <figure className="img-animal-real">
+                    <Image className="img" src={"/animais-reais/tigre-dente-de-sabre2.webp.jpeg"} alt="Tigre-Dente-de-Sabre" width={200} height={200} />
+                    <figcaption>Tigre-Dente-de-Sabre</figcaption>
+                    <figcaption className="info"><strong>Nome científico:</strong> Smilodon fatalis</figcaption>
+                    <figcaption className="info"><strong>O que era:</strong> Um felino predador famoso pelos dentes gigantes em forma de sabre.</figcaption>
+                    <figcaption className="info">
+                        <strong>Tamanho:</strong>
+                        <ul>
+                            <li>cerca de 1 metro de altura</li>
+                            <li>podia pesar até 300 kg</li>
+                        </ul>
+                    </figcaption>
+                    <figcaption className="info">
+                        <strong>Características:</strong>
+                        <ul>
+                            <li>presas de até 20cm</li>
+                            <li>corpo muito forte e musculoso</li>
+                            <li>especializado em ataques de emboscada</li>
+                        </ul>
+                    </figcaption>
+                    <figcaption className="info"><strong>Onde vivia:</strong> América do Norte e América do Sul</figcaption>
+                    <figcaption className="info"><strong>Alimentação:</strong> Era carnívoro. Caçava grandes mamíferos, como preguiças gigantes, bisões e filhotes de mamute.</figcaption>
+                    <figcaption className="info"><strong>Curiosidade:</strong> Muitos fósseis foram encontrados em La Brea Tar Pits, um local onde animais ficavam presos em piche natural.</figcaption>
                 </figure>
             </section>
-
-        </div>
+        </div >
     )
 }
--- app/museu/fosseis/fosseis.jsx
+++ app/museu/fosseis/fosseis.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link"
 import Image from "next/image"
 
 export default function fosseis() {
@@ -8,42 +9,92 @@ export default function fosseis() {
                 <figure>
                     <Image className="img" src={"/img-fosseis-certas/fossil-elasmotherium.jpeg"} alt="Fóssil de Elasmotherium" width={200} height={200} />
                     <figcaption>Fóssil de Elasmotherium</figcaption>
+                    <ul className="info">
+                        <li><figcaption><strong>Achado em:</strong> Pavlodar, Cazaquistão - 2016</figcaption></li>
+                        <li><figcaption><strong>Idade:</strong> aprox. 39 mil a 29 mil anos</figcaption></li>
+                        <li><figcaption><strong>Fonte:</strong> <Link href={"https://commons.wikimedia.org/wiki/File:Elasmotherium_sibiricum_skull_7.JPG"} target="_blank" rel="noopener noreferrer">commons.wikimedia.org</Link></figcaption></li>
+                    </ul>
                 </figure>
                 <figure>
                     <Image className="img" src={"/img-fosseis-certas/fossil-glyptodon.jpeg"} alt="Fóssil de Glyptodon" width={200} height={200} />
                     <figcaption>Fóssil de Glyptodon</figcaption>
+                    <ul className="info">
+                        <li><figcaption><strong>Achado em:</strong> Argentina - 23 de junho de 2011</figcaption></li>
+                        <li><figcaption><strong>Idade:</strong> aprox. 11 mil anos</figcaption></li>
+                        <li><figcaption><strong>Fonte:</strong> <Link href={"https://commons.wikimedia.org/wiki/File:Glyptodon_clavipes_01.jpg"} target="_blank" rel="noopener noreferrer">commons.wikimwdia.org</Link></figcaption></li>
+                    </ul>
                 </figure>
                 <figure>
                     <Image className="img" id="precisa-rotacionar" src={"/img-fosseis-certas/fossil-leao-das-cavernas.jpeg"} alt="Fóssil de Leão-das-Cavernas" width={200} height={200} />
                     <figcaption>Fóssil de filhote de Leão-das-Cavernas</figcaption>
+                    <ul className="info">
+                        <li><figcaption><strong>Achado em:</strong> Sibéria</figcaption></li>
+                        <li><figcaption><strong>Idade:</strong> mais de 10 mil anos</figcaption></li>
+                        <li><figcaption><strong>Fonte:</strong> <Link href={"https://www.nationalgeographicbrasil.com/historia/filhotes-de-leao-das-cavernas-sao-encontrados-congelados-na-siberia"} target="_blank" rel="noopener noreferrer">National Geographic Brasil</Link></figcaption></li>
+                    </ul>
                 </figure>
                 <figure>
                     <Image className="img" src={"/img-fosseis-certas/fossil-lobo-terrivel.jpeg"} alt="Fóssil de Lobo-Terrível" width={200} height={200} />
                     <figcaption>Fóssil de Lobo-Terrível</figcaption>
+                    <ul className="info">
+                        <li><figcaption><strong>Achado em:</strong> Califórnia, Estados Unidos</figcaption></li>
+                        <li><figcaption><strong>Idade:</strong> aprox. 10 a 13 mil anos</figcaption></li>
+                        <li><figcaption><strong>Fonte:</strong> <Link href={"https://www.flickr.com/photos/jsjgeology/31936676740/in/photostream/"} target="_blank" rel="noopener noreferrer">Flickr</Link></figcaption></li>
+                    </ul>
                 </figure>
                 <figure>
                     <Image className="img" src={"/img-fosseis-certas/fossil-mamute-lanoso.jpeg"} alt="Fóssil de Mamute-Lanoso" width={200} height={200} />
                     <figcaption>Fóssil de bebê de Mamute-Lanoso</figcaption>
+                    <ul className="info">
+                        <li><figcaption><strong>Achado em:</strong> Yukon, Canadá - 21 de junho de 2022</figcaption></li>
+                        <li><figcaption><strong>Idade:</strong> mais de 30 mil anos</figcaption></li>
+                        <li><figcaption><strong>Fonte:</strong> <Link href={"https://revistagalileu.globo.com/Ciencia/noticia/2022/06/mineradores-acham-filhote-de-mamute-de-30-mil-anos-mumificado-no-canada.html"} target="_blank" rel="noopener noreferrer">Revista Galileu</Link></figcaption></li>
+                    </ul>
                 </figure>
                 <figure>
                     <Image className="img" src={"/img-fosseis-certas/fossil-mandibula-mastodonte.jpeg"} alt="Fóssil de mandíbula de um Mastodonte" width={200} height={200} />
                     <figcaption>Fóssil de mandíbula de Mastodonte</figcaption>
+                    <ul className="info">
+                        <li><figcaption><strong>Achado em:</strong> Santa Catarina, Brasil - 2018</figcaption></li>
+                        <li><figcaption><strong>Idade:</strong> cerca de 30 mil anos</figcaption></li>
+                        <li><figcaption><strong>Fonte:</strong> <Link href={"https://g1.globo.com/sc/santa-catarina/noticia/fossil-de-elefante-pre-historico-e-encontrado-por-pescadores-em-sombrio-sc.ghtml"} target="_blank" rel="noopener noreferrer">g1</Link></figcaption></li>
+                    </ul>
                 </figure>
                 <figure>
                     <Image className="img" src={"/img-fosseis-certas/fossil-megaterio.jpeg"} alt="Fóssil de um Megatéiro" width={200} height={200} />
                     <figcaption>Fóssil de Megatério</figcaption>
+                    <ul className="info">
+                        <li><figcaption><strong>Achado em:</strong> 1830</figcaption></li>
+                        <li><figcaption><strong>Idade:</strong> cerca de 8 mil anos</figcaption></li>
+                        <li><figcaption><strong>Fonte:</strong> <Link href={"https://www.nhm.ac.uk/discover/what-was-megatherium.html"} target="_blank" rel="noopener noreferrer">Natural History Museum</Link></figcaption></li>
+                    </ul>
                 </figure>
                 <figure>
                     <Image className="img" src={"/img-fosseis-certas/fossil-rinoceronte-lanoso.jpeg"} alt="Fóssil de Rinoceronte-Lanoso" width={200} height={200} />
                     <figcaption>Fóssil de Rinoceronte-Lanoso</figcaption>
+                    <ul className="info">
+                        <li><figcaption><strong>Achado em:</strong> Sibéria - 2020</figcaption></li>
+                        <li><figcaption><strong>Idade:</strong> 32 mil anos</figcaption></li>
+                        <li><figcaption><strong>Fonte:</strong> <Link href={"https://www.correiobraziliense.com.br/ciencia-e-saude/2024/10/6956310-cientistas-encontram-fossil-de-rinoceronte-de-32-mil-anos-intacto.html"} target="_blank" rel="noopener noreferrer">Correio Braziliense</Link></figcaption></li>
+                    </ul>
                 </figure>
                 <figure>
                     <Image className="img" src={"/img-fosseis-certas/fossil-tigre-dentes-de-sabre.jpeg"} alt="Fóssil de Tigre-Dentes-de-Sabre" width={200} height={200} />
                     <figcaption>Fóssil de Tigre-Dentes-de-Sabre</figcaption>
+                    <ul className="info">
+                        <li><figcaption><strong>Achado em:</strong> Estados Unidos - 2019</figcaption></li>
+                        <li><figcaption><strong>Idade:</strong> 40 milhões de anos</figcaption></li>
+                        <li><figcaption><strong>Fonte:</strong> <Link href={"http://g1.globo.com/mundo/noticia/2020/12/09/esqueleto-de-tigre-dentes-de-sabre-e-vendido-por-us-842-mil-em-leilao.ghtml"} target="_blank" rel="noopener noreferrer">g1</Link></figcaption></li>
+                    </ul>
                 </figure>
                 <figure>
                     <Image className="img" src={"/img-fosseis-certas/fossil-urso-das-cavernas.jpeg"} alt="Fóssil de Urso-das-Cavernas" width={200} height={200} />
                     <figcaption>Fóssil de Urso-das-Cavernas</figcaption>
+                    <ul className="info">
+                        <li><figcaption><strong>Achado em:</strong> Rússia - 2020</figcaption></li>
+                        <li> <figcaption><strong>Idade:</strong> mais de 22 mil anos</figcaption></li>
+                        <li><figcaption><strong>Fonte:</strong> <Link href={"https://revistagalileu.globo.com/Ciencia/Arqueologia/noticia/2020/09/urso-das-cavernas-preservado-por-mais-de-22-mil-anos-e-encontrado-na-russia.html"} target="_blank" rel="noopener noreferrer">Revista Galileu</Link></figcaption></li>
+                    </ul>
                 </figure>
             </section>
 
